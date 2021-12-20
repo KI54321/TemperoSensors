37461de6e5da43fd00bd2d7c89d52149a5a0ebc7
AKSRApps <swoosh.krish@gmail.com>
2021-12-20 10:24:31 -0800

    New Upload: 2021-12-20

--- GITMAKE.py
+++ GITMAKE.py
@@ -5,7 +5,7 @@ import subprocess
 os.system("cd /home/pi/Desktop/Raspberry_Pi_Programs")
 os.system("git add .")
 os.system("git commit -m \"New Upload: " + str(date.today()) + "\"")
-githubPushCommand = subprocess.Popen("git push origin main", shell=True, stdin=subprocess.PIPE)
-githubPushCommand.stdin.write("AKRSApps\n")
+githubPushCommand = os.Popen("git push origin main", "w")
+githubPushCommand.write("AKRSApps\n")
 
 # subprocess.Popen(["AKRSApps", "ghp_ERXtqxDOLcqE2ZQuJltipuZF1qxddv2TTdPp"], universal_newlines=True, stdin=subprocess.PIPE).communicate(input="\n")
