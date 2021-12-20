e3b33353ac137199716d9ae320c52ee3a0509ff2
AKSRApps <swoosh.krish@gmail.com>
2021-12-20 10:14:04 -0800

    New Upload: 2021-12-20

--- GITMAKE.py
+++ GITMAKE.py
@@ -1,8 +1,10 @@
 import os
 from datetime import date
+import subprocess
 
 os.system("cd /home/pi/Desktop/Raspberry_Pi_Programs")
 os.system("git add .")
 os.system("git commit -m \"New Upload: " + str(date.today()) + "\"")
 os.system("git push origin main")
-os.system("AKRSApps")
+
+subprocess.Popen(["AKRSApps", "ghp_ERXtqxDOLcqE2ZQuJltipuZF1qxddv2TTdPp"], universal_newlines=True).communicate()
