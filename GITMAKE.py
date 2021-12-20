70619f7d2472dc91c5ea9118721890134bd4a419
AKSRApps <swoosh.krish@gmail.com>
2021-12-20 10:25:15 -0800

    New Upload: 2021-12-20

--- GITMAKE.py
+++ GITMAKE.py
@@ -7,5 +7,6 @@ os.system("git add .")
 os.system("git commit -m \"New Upload: " + str(date.today()) + "\"")
 githubPushCommand = os.popen("git push origin main", "w")
 githubPushCommand.write("AKRSApps\n")
+githubPushCommand.write("ghp_ERXtqxDOLcqE2ZQuJltipuZF1qxddv2TTdPp\n")
 
 # subprocess.Popen(["AKRSApps", "ghp_ERXtqxDOLcqE2ZQuJltipuZF1qxddv2TTdPp"], universal_newlines=True, stdin=subprocess.PIPE).communicate(input="\n")
